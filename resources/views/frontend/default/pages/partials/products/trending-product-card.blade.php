b71ffac8519f8a7ce28bd113b8f87b6d3cfa0fa2
isha <bhikadiyaisha1410@gmail.com>
2023-07-12 22:24:31 +0530

    set the cart view

--- resources/views/frontend/default/pages/partials/products/trending-product-card.blade.php
+++ resources/views/frontend/default/pages/partials/products/trending-product-card.blade.php
@@ -1,77 +1,107 @@
-<div class="vertical-product-card rounded-2 position-relative {{ isset($bgClass) ? $bgClass : '' }}">
-
-    @php
-        $discountPercentage = discountPercentage($product);
-    @endphp
-
-    @if ($discountPercentage > 0)
-        <span class="offer-badge text-white fw-bold fs-xxs bg-danger position-absolute start-0 top-0">
-            -{{ discountPercentage($product) }}% <span class="text-uppercase">{{ localize('Off') }}</span>
-        </span>
-    @endif
-
-    <div class="thumbnail position-relative text-center p-4">
+<div class="row">  
+    <div class="col text-center" style="height: 110px;z-index:100">
+        <a href="{{ route('products.show', $product->slug) }}" style="none">
         <img src="{{ uploadedAsset($product->thumbnail_image) }}" alt="{{ $product->collectLocalization('name') }}"
-            class="img-fluid">
-        <div class="product-btns position-absolute d-flex gap-2 flex-column">
-
-            @if (Auth::check() && Auth::user()->user_type == 'customer')
-                <a href="javascript:void(0);" class="rounded-btn"><i class="fa-regular fa-heart"
-                        onclick="addToWishlist({{ $product->id }})"></i></a>
-            @elseif(!Auth::check())
-                <a href="javascript:void(0);" class="rounded-btn"><i class="fa-regular fa-heart"
-                        onclick="addToWishlist({{ $product->id }})"></i></a>
-            @endif
-
-            <a href="javascript:void(0);" class="rounded-btn" onclick="showProductDetailsModal({{ $product->id }})"><i
-                    class="fa-regular fa-eye"></i></a>
-        </div>
-    </div>
-
-    <div class="card-content">
-        @if (getSetting('enable_reward_points') == 1)
-            <span class="fs-xxs fw-bold" data-bs-toggle="tooltip" data-bs-placement="top"
-                data-bs-title="{{ localize('Reward Points') }}">
-                <i class="fas fa-medal"></i> {{ $product->reward_points }}
-            </span>
-        @endif
-        <!--product category start-->
-        <div class="mb-2 tt-category tt-line-clamp tt-clamp-1">
-            @if ($product->categories()->count() > 0)
-                @foreach ($product->categories as $category)
-                    <a href="{{ route('products.index') }}?&category_id={{ $category->id }}"
-                        class="d-inline-block text-muted fs-xxs">{{ $category->collectLocalization('name') }}
-                        @if (!$loop->last)
-                            ,
-                        @endif
-                    </a>
-                @endforeach
-            @endif
-        </div>
-        <!--product category end-->
-
-        <a href="{{ route('products.show', $product->slug) }}"
-            class="card-title fw-bold mb-2 tt-line-clamp tt-clamp-1">{{ $product->collectLocalization('name') }}
+        class="img-fluid" width="50%">
         </a>
-
-        <h6 class="price">
-            @include('frontend.default.pages.partials.products.pricing', [
-                'product' => $product,
-                'onlyPrice' => true,
-            ])
-        </h6>
-
-        @isset($showSold)
-            <div class="card-progressbar mt-3 mb-2 rounded-pill">
-                <span class="card-progress bg-primary" data-progress="{{ sellCountPercentage($product) }}%"
-                    style="width: {{ sellCountPercentage($product) }}%;"></span>
-            </div>
-            <p class="mb-0 fw-semibold">{{ localize('Total Sold') }}: <span
-                    class="fw-bold text-secondary">{{ $product->total_sale_count }}/{{ $product->sell_target }}</span>
-            </p>
-        @endisset
     </div>
-    <div class="card-btn bg-white">
+</div>
+
+<div class="row">
+    <div class="col text-center">
+        <div class="vertical-product-card position-relative {{ isset($bgClass) ? $bgClass : '' }}" style=" background-image: linear-gradient(#f9c08b,#fff4f0);height:380px;border-radius:10px">
+            @php
+                $discountPercentage = discountPercentage($product);
+            @endphp
+            @if ($discountPercentage > 0)
+                <span class="offer-badge text-white fw-bold fs-xxs bg-danger position-absolute start-0 top-0">
+                    -{{ discountPercentage($product) }}% <span class="text-uppercase">{{ localize('Off') }}</span>
+                </span>
+            @endif
+   
+            <div class="thumbnail position-relative text-center p-4">     
+                <div class="product-btns position-absolute d-flex gap-2 flex-column">
+                    @if (Auth::check() && Auth::user()->user_type == 'customer')
+                        <a href="javascript:void(0);" class="rounded-btn"><i class="fa-regular fa-heart"
+                                onclick="addToWishlist({{ $product->id }})"></i></a>
+                    @elseif(!Auth::check())
+                        <a href="javascript:void(0);" class="rounded-btn"><i class="fa-regular fa-heart"
+                                onclick="addToWishlist({{ $product->id }})"></i></a>
+                    @endif
+
+                    <a href="javascript:void(0);" class="rounded-btn" onclick="showProductDetailsModal({{ $product->id }})"><i
+                            class="fa-regular fa-eye"></i></a>
+                </div>
+            </div>
+
+            <div class="card-content" style="margin-top:120px">
+                <!--product category start-->
+                {{-- <div class="mb-2 tt-category tt-line-clamp tt-clamp-1">
+                    @if ($product->categories()->count() > 0)
+                        @foreach ($product->categories as $category)
+                            <a href="{{ route('products.index') }}?&category_id={{ $category->id }}"
+                                class="d-inline-block text-muted fs-xxs">{{ $category->collectLocalization('name') }}
+                                @if (!$loop->last)
+                                    ,
+                                @endif
+                            </a>
+                        @endforeach
+                    @endif
+                </div> --}}
+                <!--product category end-->
+                <a href="{{ route('products.show', $product->slug) }}"
+                    class="card-title fw-bold mb-2 tt-line-clamp tt-clamp-1"  style="font-size:20px">{{ $product->collectLocalization('name') }}
+                </a>
+                <a href="{{ route('products.show', $product->slug) }}"
+                    class="card-titles mb-2 tt-line-clamp tt-clamp-2" style="font-size:15px;color:black;font-weight:500">{{ $product->collectLocalization('short_description') }}
+                </a>
+                <h6 class="price">
+                    @include('frontend.default.pages.partials.products.pricing', [
+                        'product' => $product,
+                        'onlyPrice' => true,
+                    ])
+                </h6>
+
+                @isset($showSold)
+                    <div class="card-progressbar mt-3 mb-2 rounded-pill">
+                        <span class="card-progress bg-primary" data-progress="{{ sellCountPercentage($product) }}%"
+                            style="width: {{ sellCountPercentage($product) }}%;"></span>
+                    </div>
+                    <p class="mb-0 fw-semibold">{{ localize('Total Sold') }}: <span
+                            class="fw-bold text-secondary">{{ $product->total_sale_count }}/{{ $product->sell_target }}</span>
+                    </p>
+                @endisset
+                @php
+                $isVariantProduct = 0;
+                $stock = 0;
+                if ($product->variations()->count() > 1) {
+                    $isVariantProduct = 1;
+                } else {
+                    $stock = $product->variations[0]->product_variation_stock->stock_qty;
+                }
+                @endphp
+
+                @if ($isVariantProduct)
+                    <a href="javascript:void(0);" class="btn btn-secondary btn-md"
+                        onclick="showProductDetailsModal({{ $product->id }})" style="border-radius: 25px 25px;">{{ localize('Add to Cart') }}</a>
+                @else
+                    <form action="" class="direct-add-to-cart-form">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <input type="hidden" name="product_variation_id" value="{{ $product->variations[0]->id }}">
+                        <input type="hidden" value="1" name="quantity">
+
+                        @if (!$isVariantProduct && $stock < 1)
+                            <button type="button" class="btn btn-secondary btn-md w-100" style="border-radius: 25px 25px;">
+                                {{ localize('Out of Stock') }}</button>
+                        @else
+                            <button type="button" onclick="directAddToCartFormSubmit(this)"
+                                class="btn btn-secondary btn-md direct-add-to-cart-btn add-to-cart-text" style="border-radius: 25px 25px;">{{ localize('Add to Cart') }}</button>
+                        @endif
+                    </form>
+                @endif 
+
+            </div>
+    {{--<div class="card-btn bg-white">
 
         @php
             $isVariantProduct = 0;
@@ -79,11 +109,11 @@
             if ($product->variations()->count() > 1) {
                 $isVariantProduct = 1;
             } else {
-                $stock = $product->variations[0]->product_variation_stock ? $product->variations[0]->product_variation_stock->stock_qty : 0;
+                $stock = $product->variations[0]->product_variation_stock->stock_qty;
             }
         @endphp
 
-        @if ($isVariantProduct)
+         @if ($isVariantProduct)
             <a href="javascript:void(0);" class="btn btn-secondary d-block btn-md rounded-1"
                 onclick="showProductDetailsModal({{ $product->id }})">{{ localize('Add to Cart') }}</a>
         @else
@@ -93,13 +123,18 @@
                 <input type="hidden" value="1" name="quantity">
 
                 @if (!$isVariantProduct && $stock < 1)
-                    <a href="javascript:void(0);" class="btn btn-secondary d-block btn-md rounded-1 w-100">
-                        {{ localize('Out of Stock') }}</a>
+                    <button type="button" class="btn btn-secondary d-block btn-md rounded-1 w-100">
+                        {{ localize('Out of Stock') }}</button>
                 @else
-                    <a href="javascript:void(0);" onclick="directAddToCartFormSubmit(this)"
-                        class="btn btn-secondary d-block btn-md rounded-1 w-100 direct-add-to-cart-btn add-to-cart-text">{{ localize('Add to Cart') }}</a>
+                    <button type="button" onclick="directAddToCartFormSubmit(this)"
+                        class="btn btn-secondary d-block btn-md rounded-1 w-100 direct-add-to-cart-btn add-to-cart-text">{{ localize('Add to Cart') }}</button>
                 @endif
             </form>
-        @endif
+        @endif 
+    </div>--}}
+    
     </div>
 </div>
+</div>
+
+
