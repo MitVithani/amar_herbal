d6b9d9dc226f8b7b683de6062b533066e9323343
isha <bhikadiyaisha1410@gmail.com>
2023-08-01 02:02:58 +0530

    set the top category section with static image

--- resources/views/frontend/default/pages/partials/home/category.blade.php
+++ resources/views/frontend/default/pages/partials/home/category.blade.php
@@ -4,6 +4,13 @@
         /* justify-content: space-evenly !important; */
         /* align-items: flex-start !important; */
     }
+    .my-gshop-category-box{
+        padding:10px 10px;
+    }
+    .gshop-animated-iconbox img:hover{
+        border: 2px dashed black;
+        border-radius: 5px;
+    }
     @media only screen and (max-width: 960px) {      
     #mycol{
         padding: 0% !important;
@@ -14,24 +21,25 @@
     <img src="{{ staticAsset('frontend/default/assets/img/shapes/bg-shape.png') }}" alt="bg shape"
         class="position-absolute bottom-0 start-0 w-100 z--1">
     <div class="container">
-        <div class="gshop-category-box border-secondary rounded-3 bg-white">
+        <div class="my-gshop-category-box border-0 rounded-3 bg-white">
             <div class="text-center section-title">
                 <h4 class="d-inline-block px-2 bg-white mb-4">{{ localize('Our Top Categories') }}</h4>
             </div>
             <div class="row g-4 myclass">
-                @php
+                {{-- @php
                     $top_category_ids = getSetting('top_category_ids') != null ? json_decode(getSetting('top_category_ids')) : [];
                     $categories = \App\Models\Category::whereIn('id', $top_category_ids)->get();
-                @endphp
+                @endphp --}}
 
-                @foreach ($categories as $category)
-                    @php
+                {{-- @foreach ($categories as $category) --}}
+                    {{-- @php
                         $productsCount = \App\Models\ProductCategory::where('category_id', $category->id)->count();
-                    @endphp
-                    <div class="col-3 col-xl-3 col-lg-3 col-md-3 col-sm-3" id="mycol">
-                        <a href="{{ route('products.index') }}?&category_id={{ $category->id }}">
-                        <div class="gshop-animated-iconbox text-center border rounded-3 position-relative overflow-hidden {{ $loop->even ? 'color-2' : '' }}">
-                            <div class="animated-icon d-inline-flex align-items-center rounded-circle position-relative">
+                    @endphp --}}
+                    <div class="col-md-3" id="mycol">
+                        {{-- <a href="{{ route('products.index') }}?&category_id={{ $category->id }}"> --}}
+                        <div class="gshop-animated-iconbox text-center position-relative overflow-hidden {{--{{ $loop->even ? 'color-2' : '' }}--}}">
+                            <img src="{{ staticAsset('frontend/default/assets/img/cate1.png') }}" class="w-100 rounded-2" alt="cate1">
+                            {{-- <div class="animated-icon d-inline-flex align-items-center rounded-circle position-relative">
                                 <img src="{{ uploadedAsset($category->collectLocalization('thumbnail_image')) }}"
                                     alt="" class="img-fluid">
                             </div>
@@ -40,14 +48,35 @@
                                 class="text-dark fs-sm fw-bold d-block mt-3">{{ $category->collectLocalization('name') }}</a>
                             <span
                                 class="total-count position-relative ps-3 fs-sm fw-medium doted-primary">{{ $productsCount }}
-                                {{ localize('Items') }}</span>
+                                {{ localize('Items') }}
+                            </span>
 
                             <a href="{{ route('products.index') }}?&category_id={{ $category->id }}"
-                                class="explore-btn position-absolute"><i class="fa-solid fa-arrow-up"></i></a>
+                                class="explore-btn position-absolute"><i class="fa-solid fa-arrow-up"></i>
+                            </a> --}}
                         </div>
-                        </a>
+                        {{-- </a> --}}
                     </div>
-                @endforeach
+
+                    <div class="col-md-3" id="mycol">
+                        <div class="gshop-animated-iconbox text-center position-relative overflow-hidden">
+                            <img src="{{ staticAsset('frontend/default/assets/img/cate4.png') }}" class="w-100 rounded-2" alt="cate4">
+                        </div>
+                    </div>
+
+                    <div class="col-md-3" id="mycol">
+                        <div class="gshop-animated-iconbox text-center position-relative overflow-hidden">
+                            <img src="{{ staticAsset('frontend/default/assets/img/cate2.png') }}" class="w-100 rounded-2" alt="cate2">
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-3" id="mycol">
+                        <div class="gshop-animated-iconbox text-center position-relative overflow-hidden">
+                            <img src="{{ staticAsset('frontend/default/assets/img/cate3.png') }}" class="w-100 rounded-2" alt="cate3"> 
+                        </div>
+                    </div>
+                    
+                {{-- @endforeach --}}
             </div>
         </div>
     </div>
